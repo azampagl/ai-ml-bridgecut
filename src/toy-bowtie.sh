b303613ecf6dbafe513cd820bdd82ba1d29d1110
Aaron Zampaglione <azampagl@azampagl.com>
2011-11-28 11:32:11 -0500

    Removed unecessary files.

--- src/toy-bowtie.sh
+++
@@ -1,4 +0,0 @@
-python main.py -i "../data/toy/toy-bowtie.txt" -o "../results/toy/toy-bowtie-edge-c.txt" -v "edge-c" -t 0
-python main.py -i "../data/toy/toy-bowtie.txt" -o "../results/toy/toy-bowtie-edge-b.txt" -v "edge-b" -t 0
-python main.py -i "../data/toy/toy-bowtie.txt" -o "../results/toy/toy-bowtie-vertex-c.txt" -v "vertex-c" -t 0
-python main.py -i "../data/toy/toy-bowtie.txt" -o "../results/toy/toy-bowtie-vertex-b.txt" -v "vertex-b" -t 0
