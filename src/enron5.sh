3e78aa50c77619723325d922bd9c2096e8096d12
Aaron Zampaglione <azampagl@azampagl.com>
2011-11-28 15:27:03 -0500

    Removing unecessary files.

--- src/enron5.sh
+++
@@ -1,4 +0,0 @@
-python main.py -i "../data/enron/enron5.txt" -o "../results/enron/enron5-edge-c.txt" -v "edge-c" -t .4
-python main.py -i "../data/enron/enron5.txt" -o "../results/enron/enron5-edge-b.txt" -v "edge-b" -t .4
-python main.py -i "../data/enron/enron5.txt" -o "../results/enron/enron5-vertex-c.txt" -v "vertex-c" -t .4
-python main.py -i "../data/enron/enron5.txt" -o "../results/enron/enron5-vertex-b.txt" -v "vertex-b" -t .4
